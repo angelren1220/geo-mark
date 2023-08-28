4f9cab292a9d23f0f9d4cf963c027972988a98b0
Linuxe <angelren1220@gmail.com>
2023-08-28 18:47:28 -0400

    add location search

--- src/components/LocationSearch.vue
+++ src/components/LocationSearch.vue
@@ -1,56 +1,33 @@
 <template>
-  <h1>{{ msg }}</h1>
   <div class="location-search">
-    <button @click="getCurrentLocation">Get Current Location</button>
-    <input type="text" v-model="searchedLocation" @keyup.enter="searchLocation" placeholder="Search for a location..." />
+    <input v-model="searchTerm" @keyup.enter="searchLocation" placeholder="Enter location" />
     <button @click="searchLocation">Search</button>
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-    msg: String
-  },
+import axios from 'axios';
+const apiKey = process.env.VUE_APP_GOOGLE_MAPS_API_KEY;
 
+export default {
   data() {
     return {
-      searchedLocation: ''
+      searchTerm: ''
     };
   },
-
   methods: {
-    async getCurrentLocation() {
-      try {
-        const position = await new Promise((resolve, reject) => {
-          navigator.geolocation.getCurrentPosition(resolve, reject);
-        });
-        const { latitude, longitude } = position.coords;
-        this.$emit('location-selected', { latitude, longitude });
-      } catch (error) {
-        console.error("Couldn't fetch current location", error);
-      }
-    },
     searchLocation() {
-      if (this.searchedLocation) {
-        this.$emit('location-searched', this.searchedLocation);
-      }
+      axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.searchTerm}&${apiKey}`).then(response => {
+        const location = response.data.results[0].geometry.location;
+        this.setLocationAndMarker(location.lat, location.lng);
+      });
     }
   }
+
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* Add your styles here */
-h1 {
-  margin: 40px 0 0;
-}
-.location-search {
-  display: flex;
-  gap: 10px;
-}
+
 </style>
-
-
-
