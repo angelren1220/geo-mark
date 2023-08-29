d196bc35c1a065c3a51a10a88b86aea6f5c7a455
Linuxe <angelren1220@gmail.com>
2023-08-29 16:43:45 -0400

    add ui styles for search

--- src/components/LocationSearch.vue
+++ src/components/LocationSearch.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="location-search">
-    <input v-model="searchTerm" @keyup.enter="searchLocation" placeholder="Enter location" />
-    <button @click="searchLocation">Search</button>
-  </div>
+  <section class="ui two column centered grid">
+    <div class="column">
+      <form class="ui segment large form">
+        <div class="ui message red"></div>
+        <div class="ui segment">
+          <div class="field location-search">
+            <input v-model="searchTerm" @keyup.enter="searchLocation" placeholder="Enter location" />
+          </div>
+          <button class="ui button" @click="searchLocation">Search</button>
+        </div>
+      </form>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -29,5 +38,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.ui.button {
+  background-color: green;
+  color: white;
+}
 </style>
