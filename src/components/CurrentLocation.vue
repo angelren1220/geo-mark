033eabdd0aa1fea167654614d916ae65e1ab07c0
Linuxe <angelren1220@gmail.com>
2023-08-29 17:50:21 -0400

    change coords to address

--- src/components/CurrentLocation.vue
+++ src/components/CurrentLocation.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="current-location">
-    <i class="map marker alternate icon location-icon" @click="getCurrentLocation"></i>
+    <button class="ui button"  @click="getCurrentLocation"><i class="map marker alternate icon location-icon"></i></button>
     <!-- Displaying the latitude and longitude -->
     <div v-if="location">
-      Latitude: {{ location.lat }}, Longitude: {{ location.lng }}
-    </div>  
+      {{ location }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+const apiKey = process.env.VUE_APP_GOOGLE_MAPS_API_KEY;
+
 export default {
 
   data() {
@@ -18,20 +21,41 @@ export default {
   },
 
   methods: {
+
     getCurrentLocation() {
-      navigator.geolocation.getCurrentPosition(position => {
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
-        // this.setLocationAndMarker(lat, lng);
-        this.location = { lat, lng };
-      });
+
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+          const lat = position.coords.latitude;
+          const lng = position.coords.longitude;
+          console.log(lat, lng);
+          this.getAddressFrom(lat, lng);
+        });
+      } else {
+        console.log("Browser dose not support Geolocation API");
+      }
+    },
+
+    getAddressFrom(lat, lng) {
+      axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + `&key=${apiKey}`)
+        .then(response => {
+          if (response.data.error_message) {
+            console.log(response.data.error_message);
+          } else {
+            this.location = response.data.results[0].formatted_address
+            console.log(this.location);
+          }
+        })
+        .catch(error => {
+          console.log(error.message);
+        });
     }
+
   }
 }
 
 </script>
 <style scoped>
-
 .location-icon:hover {
   color: red;
 }
